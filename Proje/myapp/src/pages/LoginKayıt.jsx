d3b0aeed7c461bbcc7a4c564ca6f89f601823202
Hafize-Nur <hafizenursari26@gmail.com>
2024-05-25 18:41:32 +0300

    commit islem

--- Proje/myapp/src/pages/LoginKayıt.jsx
+++ Proje/myapp/src/pages/LoginKayıt.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import logo from '../logolar/homelogo.png';
 
 function LoginKayit() {
-  const [activeTab, setActiveTab] = useState('login'); // Aktif sekmeyi yönetmek için state
+  const [activeTab, setActiveTab] = useState('login');
 
   const handleTabChange = (tabName) => {
     setActiveTab(tabName);
@@ -13,31 +13,27 @@ function LoginKayit() {
 
   return (
     <div className="login-kayit">
-         <header className="App-header">
-          <img className="header-logo"src={logo} alt='logo' />
-        <nav className="header-nav">  {/* New navigation element */}
+      <header className="App-header">
+        <img className="header-logo" src={logo} alt="logo" />
+        <nav className="header-nav">
           <ul className="categories">
             <li><Link to="/kategoriler/elektronik">Elektronik</Link></li>
             {/* ... */}
           </ul>
         </nav>
-      <div className='Arama'>
-      <input type="search" placeholder='Ara...' className='AramaCubugu'/>
-        <button className='Ara' type='submit'> Ara</button>
-      </div>
-   
-        <div className='headerButtons'> 
-        
-        <Link to="/LoginKayıt" className='headerGirisKayıt'>Giris-Kayıt</Link>
-        <Link to="/Sepetim" className='sepet'>Sepetim</Link>
-        <button className='menu-toggle'>☰</button>
+        <div className='Arama'>
+          <input type="search" placeholder='Ara...' className='AramaCubugu' />
+          <button className='Ara' type='submit'>Ara</button>
         </div>
-        
+        <div className='headerButtons'>
+          <Link to="/LoginKayıt" className='headerGirisKayıt'>Giris-Kayıt</Link>
+          <Link to="/Sepetim" className='sepet'>Sepetim</Link>
+          <button className='menu-toggle'>☰</button>
+        </div>
       </header>
-     
       <div className="container">
         <div className="tabs">
-          <button 
+          <button
             className={`tab ${activeTab === 'login' ? 'active' : ''}`}
             onClick={() => handleTabChange('login')}
           >
@@ -63,7 +59,6 @@ function LoginKayit() {
   );
 }
 
-// Login Form
 function LoginForm({ onLogin }) {
   const [formData, setFormData] = useState({
     email: '',
@@ -105,7 +100,6 @@ function LoginForm({ onLogin }) {
           required
         />
       </div>
-
       <div className="password">
         <input
           type="password"
@@ -116,7 +110,6 @@ function LoginForm({ onLogin }) {
           required
         />
       </div>
-
       <button className="buttonGiris" type="submit">
         Giriş Yap
       </button>
@@ -125,7 +118,6 @@ function LoginForm({ onLogin }) {
   );
 }
 
-// Kayıt Formu
 function KayitForm({ onShowLogin }) {
   const [formData, setFormData] = useState({
     name: '',
@@ -141,124 +133,119 @@ function KayitForm({ onShowLogin }) {
       ...prevState,
       [name]: value,
     }));
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        let hata = false;
-        if (
-          formData.name === '' ||
-          formData.email === '' ||
-          formData.sifre === '' ||
-          formData.telefon === ''
-        ) {
-          hata = true;
-        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-          hata = true;
-        }
-    
-        if (hata) {
-          alert('Tüm alanları doldurmanız ve geçerli bir e-posta girmeniz gerekmektedir.');
-        } else {
-          // **Sunucuya veri gönderme işlemini buraya ekleyin**
-    
-          // Örnek: Fetch API kullanarak veri gönderme
-    
-          const data = {
-            name: formData.name,
-            email: formData.email,
-            sifre: formData.sifre,
-            telefon: formData.telefon,
-          };
-    
-          fetch('/Presentation/Controller/CustomerController.cs', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-          })
-            .then((response) => response.json())
-            .then((responseData) => {
-              if (responseData.success) {
-                // Kayıt işlemi sonrası yapılacak işlemleri buraya ekleyin
-                console.log('Kayıt işlemi başarılı!');
-                alert('Kayıt işlemi başarıyla tamamlandı!');
-                onShowLogin(); // Giriş formuna geçiş
-              } else {
-                // Hata mesajı gösterin
-                console.log('Kayıt işlemi başarısız!');
-                setErrorMessage(responseData.error); // Hata mesajını state'e ekleyin
-              }
-            });
-    
-          // **Sunucuya veri gönderme işleminin sonu**
-        }
-      };
-    
-      return (
-        <form onSubmit={handleSubmit}>
-          <div className="isim">
-            <label htmlFor="isim">Adı:</label>
-            <input
-              type="text"
-              id="isim"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Adınızı girin"
-              required
-            />
-          </div>
-    
-          <div className="email">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Emailinizi girin"
-              required
-            />
-          </div>
-    
-          <div className="sifre">
-            <label htmlFor="sifre">Şifre:</label>
-            <input
-              type="password"
-              id="sifre"
-              name="sifre"
-              value={formData.sifre}
-              onChange={handleChange}
-              placeholder="Şifrenizi girin"
-              required
-            />
-          </div>
-    
-          <div className="telefon">
-            <label htmlFor="telefon">Telefon Numarası:</label>
-            <input
-              type="text"
-              id="telefon"
-              name="telefon"
-              value={formData.telefon}
-              onChange={handleChange}
-              placeholder="Telefon numaranızı girin"
-              required
-            />
-          </div>
-    
-          {errorMessage && <p className="hata-mesajı">{errorMessage}</p>}
-    
-          <button type="submit" onClick={handleSubmit} className="buttonKayit">
-            Kaydol
-          </button>
-        </form>
-      );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let hata = false;
+    if (
+      formData.name === '' ||
+      formData.email === '' ||
+      formData.sifre === '' ||
+      formData.telefon === ''
+    ) {
+      hata = true;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      hata = true;
     }
-    
-    export default LoginKayit;
-    
+
+    if (hata) {
+      alert('Tüm alanları doldurmanız ve geçerli bir e-posta girmeniz gerekmektedir.');
+    } else {
+      const data = {
+        name: formData.name,
+        email: formData.email,
+        sifre: formData.sifre,
+        telefon: formData.telefon,
+      };
+
+      fetch('/Presentation/Controller/CustomerController.cs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((responseData) => {
+          if (responseData.success) {
+            console.log('Kayıt işlemi başarılı!');
+            alert('Kayıt işlemi başarıyla tamamlandı!');
+            onShowLogin(); // Giriş formuna geçiş
+          } else {
+            console.log('Kayıt işlemi başarısız!');
+            setErrorMessage(responseData.error);
+          }
+        })
+        .catch((error) => {
+          console.error('There was a problem with the fetch operation:', error);
+          setErrorMessage('Kayıt işlemi sırasında bir hata oluştu.');
+        });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="isim">
+        <label htmlFor="isim">Adı:</label>
+        <input
+          type="text"
+          id="isim"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Adınızı girin"
+          required
+        />
+      </div>
+      <div className="email">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Emailinizi girin"
+          required
+        />
+      </div>
+      <div className="sifre">
+        <label htmlFor="sifre">Şifre:</label>
+        <input
+          type="password"
+          id="sifre"
+          name="sifre"
+          value={formData.sifre}
+          onChange={handleChange}
+          placeholder="Şifrenizi girin"
+          required
+        />
+      </div>
+      <div className="telefon">
+        <label htmlFor="telefon">Telefon Numarası:</label>
+        <input
+          type="text"
+          id="telefon"
+          name="telefon"
+          value={formData.telefon}
+          onChange={handleChange}
+          placeholder="Telefon numaranızı girin"
+          required
+        />
+      </div>
+      {errorMessage && <p className="hata-mesajı">{errorMessage}</p>}
+      <button type="submit" className="buttonKayit">
+        Kaydol
+      </button>
+    </form>
+  );
+}
+
+export default LoginKayit;
